ee8ab545e3285be55cb82b69b27c5c65e1b4b5a6
parteek kumar <digitalparteek@gmail.com>
2023-08-22 11:19:31 +0530

    added code

---
+++ src/components/Todos.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { useSelector } from "react-redux";
+// useselector is the one hook which will be responsible for
+// reading from the store
+
+function Todos() {
+  return <div>Todos</div>;
+}
+
+export default Todos;
--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -4,7 +4,7 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-  todos: [],
+  todos: [{ id: 1, text: "Hello", completed: false }],
 };
 
 export const todoSlice = createSlice({
